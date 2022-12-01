0071faf31a0a8036ff46051080a6528e35bdecc7
Benjamin CAVY <benjamin.cavy@maif.fr>
2022-12-01 22:14:25 +0100

    done

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ type ButterEvent =
   | { type: "butterSelected"; butter: string };
 
 const butterMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QCMCuAXdYBOA6AdgPYAiYADgIbZYC2Y+6AxBOVbfegMpgA2YAxlggBtAAwBdRKDKFYAS3RzC+KSAAeiALQA2AMy7cAFl0BWAEwB2MyYCMF3TYAchgDQgAnojNntuG3osrQxNRCxNtRwBfSLc0TBxcHkIKCDl8KAAhDCxsZmUwXDSAN0IAawK4nMTk1PSs+OwEYsJ+CkVlMXFO1Rl5dpUkdS1tUV8LR1FRAE4pmynLEdcPRENA3D0TR0XjGxtzaNjshKJ6nMZKnG4+QUhuwd6FJQHQDQRNE3ncRydvi2NZqb6NyeBDabQWXC6QxTRwWUa-bQ+A4gC54IoUHhyCCMWCoZA0BR3aSyR7KVSvd5goxWRwhHzWGG6YGIRy0owOfSGOwWbQmBzI1G4XH4hSKdJ5fAFZrlXCC4UEzBpKBNfAlVr9TpEkAPfrkrSGb5+XRmUQmf4LbQ2ZkIGyjUSQxGiQyiE1OMy6AVHPDy0VKxg4bCEPBkHhtABmQZosq9QrxCrFyua6qemokPRJusGFMCjlwJh5NLNU0Mzqm1qmtiM0K2UwsszB4M9DVwAaDjDUsHQbQKFDDOQAFLpJqIAJTnGOt7BanVPPVvCy7XB1p2mwzu7QwszWj5mL4+aFm1kG1bRGIgIgseCDVHpvqzrNaRw+PMFsy06El6bWhe4F3jLYls6YQGk2VREKQlDUGAdAMLepLPEMbyWhCgRTJajj6Fs4KONa-j2n+rLaIBoSHqBCRJCkSqnDgcGZi8ww2AYxpgmYMy6ACULWqYhhLrahi8n8sxOlEZ6CicXq0fe9FvOEuY2JYa6MchcxLCCmwQrs6HgoEYSjGRaIYliklkg+MkbpCug8v4Pj2E4JjWhMEKwkO7owhE-gmPpsYioq6TGQhFKbFMuDun8oxcgsTrlpsuAwmhJa6BEBpoV5PrxBA-lzpoxi7nY+gjD8ExmFayw2v4PFWLyilFiBokTtggbYJlpmaLp6x-NYfILtMug4aV3WQtW9hbKMgQ2KekRAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QCMCuAXdYBOA6AdgPYAiYADgIbZYC2Y+6AxBOVbfegMpgA2YAxlggBtAAwBdRKDKFYAS3RzC+KSAAeiALQA2ABwAWXAFZdATn27dogIwAma9YA0IAJ6Jbe3AHYbAZh-Woj6iegC+oc5omDi4AG4UPHIQjLCoyDQKYpJIIDLyisqqGgiaDrpe3tom1qY2tkamur7Obgi6Rka4vu2+vka2utr6JvrhkRhYeKnpCor4UMzKYLhy+LGEANbLUZO40xmYq1AIq+v8FAX4WVmqeQpKKjnFpbZBxqbd+tqfpka+2i1EKZtBV-tpREELNpTF5TLYxiAdjF9rMjowcNhCHgyDwLgAzLE0XBIqZpA5zY6nQjnS7XCS3WT3QpPLTWfy2XDaOzckyvXx2QEIKqiYyDAa9KqWDoIkm4DFYxhqWDoC7LCh4yYACl8ENEAEpGLL5dgbjk7pciqzrF5tN4IfpfMDRL12rpBUZPKZTA49L59PavPCIoiJjESdw+IIHot8MtlariaG8OHeAJafSzYyLSySoFgbg2bZel9vbCvEZBV4rF0Pg1rPpzLprdYZUnE9FsBG0w927seIQKBAjgAhJMx5ZUra9sNJrtR5TTvD9wcjpMnNbUi4POnZaRZh6WkrlCodD1DEL1d0erp2fr9L0hfQt4OylORy6LgiEUcdw2z1OCJApp7vkB45jo-S4E2T5DEWXiBH8gogroN62PUJgNP4Rith2i5zh+qwEtgNBbgu6DYBQ+DyBwjDAbk+7MqAzyBr4uAWEYXjwUEohGNY7SCoERa4I05bQta-KcUG4y4W+3YLoRhKkfguDkZR1EMLR1i7vRoGMeoWjwRU1hVH8XpGEEHheAJbIVA2OqOvoDY8VyOG7LJ87KQpxFKXEcjyMgfCMF5JGXAAwv2sBARmIFMo8TFaB0KF8cZzpco6bJOK4iBlCh3yiAMMJskYT5eOEwZECw8A5CSDK6XF+m5voHgFtoti1OKfICllCDWNeAy8Q2rU2tCRauTERCkJQ1BgHQDC1bFh6aN6IoNOZFj5VyyGCrYwy4P19bArYw1wr4Y14PEiQQPN2bxbmrzWMJjq-HxsJteWlawsJQRNA6PgfA4Z17GSqLzNdYG3aU-hJa17VNJ1tiCu0to+IM9YdU9gMotEV2ZnVi12G8VTfF8Px-AC3WBN8oqibxogwSCgPGmDenPGy-xdDq5S9bo8Pup63rGT9AZSSGMn-u+4M6Qt4Ewqxp7aOerUVt1-gVFWAtNMZsEi6+4tycpNW49LENwp08uK5e3VfMjZhlPyCtNTrbbuR+srLkO8w-pMzP1c8VQclWHgGK1CsfPoV6dOrojHg+QzPtJbl6x5n5EF7OA+4tPMcuZfrWsC5TK60egB3xJMeDUaFO2LHb4T2soQEsGfgd8picrxjRWNymWtE2D0cTUViwk1f2Ay7PbBUpTcm0J+jwdaBgV74lutP9wkF2JXgSYGo9JwR+BESFPaqVRcgcFPDWaAXbFz1WT4ncv2X5SKIkeqWW9V4nNcAXvB8+bEflyACmAc+zwGwnjsL8GEjRrTwQEk-Neok34wI-jOL+Et5L70Uh+AAFkkFg9VzSS2eLxCoIQbSzzak2Ti3dsr9U5AMZsAwGwfG0GVUIQA */
   createMachine<ButterContext, ButterEvent>(
     {
       context: {
@@ -45,27 +45,8 @@ const butterMachine =
         noDepartement: {
           on: {
             departementSelected: {
-              target: "loadingButter",
+              target: "butterSelection",
               actions: assign({ departement: (ctx, evt) => evt.departement }),
-            },
-          },
-        },
-        loadingButter: {
-          invoke: {
-            src: "fetchButters",
-            onDone: [
-              {
-                target: "noButter",
-                actions: assign({ butters: (ctx, evt) => evt.data }),
-              },
-            ],
-          },
-        },
-        noButter: {
-          on: {
-            butterSelected: {
-              target: "valid",
-              actions: assign({ butterType: (ctx, evt) => evt.butter }),
             },
           },
         },
@@ -92,13 +73,77 @@ const butterMachine =
             ],
           },
         },
-        submitted: {},
+        submitted: {
+          type: "final",
+        },
         error: {
           after: {
             "3000": {
-              target: "#butter.noButter",
+              target: "#butter.butterSelection.butter.noButter",
+              actions: [],
               internal: false,
             },
+          },
+        },
+        butterSelection: {
+          type: "parallel",
+          states: {
+            butter: {
+              initial: "loadingButter",
+              states: {
+                loadingButter: {
+                  invoke: {
+                    src: "fetchButters",
+                    onDone: [
+                      {
+                        target: "noButter",
+                        actions: assign({ butters: (ctx, evt) => evt.data }),
+                      },
+                    ],
+                  },
+                },
+                noButter: {
+                  on: {
+                    butterSelected: {
+                      target: "done",
+                      actions: assign({ butterType: (ctx, evt) => evt.butter }),
+                    },
+                  },
+                },
+                done: {
+                  type: "final",
+                },
+              },
+            },
+            information: {
+              initial: "transient",
+              states: {
+                transient: {
+                  always: [
+                    {
+                      target: "visible",
+                      cond: "outsideBrittany",
+                    },
+                    {
+                      target: "hidden",
+                    },
+                  ],
+                },
+                visible: {
+                  on: {
+                    informationClosed: {
+                      target: "hidden",
+                    },
+                  },
+                },
+                hidden: {
+                  type: "final",
+                },
+              },
+            },
+          },
+          onDone: {
+            target: "valid",
           },
         },
       },
@@ -134,7 +179,11 @@ const butterMachine =
           });
         },
       },
-      guards: {},
+      guards: {
+        outsideBrittany: (context) => {
+          return !["22", "56", "35", "29"].includes(context.departement || "");
+        },
+      },
     }
   );
 
@@ -145,7 +194,7 @@ function App() {
 
   service.subscribe((state) => {
     if (
-      state.matches("butterSelection.information.displayed") &&
+      state.matches("butterSelection.information.visible") &&
       !dialogRef.current?.open
     ) {
       dialogRef.current?.showModal();
@@ -211,7 +260,12 @@ function App() {
             <Loader />
           ) : (
             context.departement && (
-              <fieldset className="butters" disabled={!matches("noButter")}>
+              <fieldset
+                className="butters"
+                disabled={
+                  !["noButter", "butterSelection.butter.noButter"].some(matches)
+                }
+              >
                 <legend>Favorite butter</legend>
                 {context.butters.map(({ id, name }) => {
                   return (
